67b20d53604fdb604a48a129fbaa894a0007d375
rohanSnippet <rohan110620@gmail.com>
2025-02-21 10:31:49 +0530

    App.jsx updated

--- supatest/src/App.jsx
+++ supatest/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Create from './Create';
 import './App.css'
 import Update from './Update';
+import SignUp from './SignUp';
 
 function App() {
   return (
@@ -12,11 +13,13 @@ function App() {
    
    <Link to="/">Home</Link>
    <Link to="/create">Create</Link>
+   <Link to="/signup">Sign Up</Link>
    </nav>
    <Routes>
     <Route index path='/' element={<Home/>}></Route>
     <Route path='/create' element={<Create/>}></Route>
     <Route path='/:id' element={<Update/> }></Route>
+    <Route path='/signup' element={<SignUp /> }></Route>
    </Routes>
    </BrowserRouter>
   )
