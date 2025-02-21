78a11de27041a42fe26784c467c4259ba46b37ab
rohanSnippet <rohan110620@gmail.com>
2025-02-21 10:32:56 +0530

    supabaseConfig.js

--- supatest/src/supabaseConfig.js
+++ supatest/src/supabaseConfig.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL; //"https://jfqampurtzclbltwgqil.supabase.co";
-const supabaseKey = import.meta.env.VITE_APP_ANON_KEY; //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmcWFtcHVydHpjbGJsdHdncWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2MjE3OTEsImV4cCI6MjA1MTE5Nzc5MX0.uzNLVUudUxUA6THHATdu5GPw1VNkLYMhjmbea0zzUx8";
+const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_APP_ANON_KEY; 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
